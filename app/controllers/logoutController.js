270928575eb7dd15200d1822ea30c85a4ee0b266
Nina Vione <nina_ovsepyan@epam.com>
2016-01-25 15:28:05 +0300

    v.0.6
    added logout logic (logoutController)

---
+++ app/controllers/logoutController.js
@@ -0,0 +1,8 @@
+app.controller('logoutController', [ '$scope', 'dataService',
+  function ($scope, dataService) {
+
+    $scope.signOut = function () {
+      dataService.removeStorageData();
+    };
+  }
+]);
